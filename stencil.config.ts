cc285f8f57ed4108d4a89938ce47fcc7c4418681
Leo Haughton <leo_haughton@trimble.com>
2022-03-25 14:07:49 +1300

    #406: Generate Angular Component Library
    This commit generates an Angular component library
    using the StencilJS Angular Framework Integration.
    
    Major design decisions:
     * There will be a new directory named
       angular-workspace in the root directory
       This has been generated using the angular
       cli and will house the generated components
     * In order for angular to gain access to
       interfaces exported inside the web component
       classes I have had to change package.json
       types proprty to point to a new file
       named interfaces.d.ts. I am not sure if
       this will cause breakages and am looking
       for Ryan et al to review this.
    
    Todo Items
     * Wrap the existing stencil workspace
       into a sub-directory to tidy up the project
     * Add a github action to deploy the components
       as part of the full publish command

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,39 +1,45 @@
-import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
-import { postcss } from '@stencil/postcss';
-import autoprefixer from 'autoprefixer';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
+import { postcss } from "@stencil/postcss";
+import { angularOutputTarget } from '@stencil/angular-output-target';
+import autoprefixer from "autoprefixer";
 
 export const config: Config = {
-  namespace: 'modus-web-components',
+  namespace: "modus-web-components",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+      type: "dist",
+      esmLoaderPath: "../loader",
     },
     {
-      type: 'dist-custom-elements',
+      type: "dist-custom-elements",
     },
     {
-      type: 'docs-readme',
-      footer: ''
+      type: "docs-readme",
+      footer: "",
     },
     {
-      type: 'www',
+      type: "www",
       serviceWorker: null, // disable service workers
     },
+    angularOutputTarget({
+      componentCorePackage: "@trimble-oss/modus-web-components",
+      directivesProxyFile: "./angular-workspace/projects/trimble-oss/modus-angular-components/src/lib/stencil-generated/components.ts",
+      directivesArrayFile: "./angular-workspace/projects/trimble-oss/modus-angular-components/src/lib/stencil-generated/index.ts",
+    }),
   ],
   plugins: [
     postcss({
-      plugins: [autoprefixer()]
+      plugins: [autoprefixer()],
     }),
     sass({
       injectGlobalPaths: [
-        'src/global/modus-functions.scss', // adds @import 'src/global/modus-functions.scss' statement
-        'src/global/modus-colors.scss', // adds @import 'src/global/modus-colors.scss' statement
-        'src/global/modus-light-theme.scss', // adds @import 'src/global/modus-light-theme.scss' statement
-        'src/global/modus-variables.scss', // adds @import 'src/global/modus-variables.scss' statement
-        'src/global/shared-mixins.scss',
-      ]
+        "src/global/modus-functions.scss", // adds @import 'src/global/modus-functions.scss' statement
+        "src/global/modus-colors.scss", // adds @import 'src/global/modus-colors.scss' statement
+        "src/global/modus-light-theme.scss", // adds @import 'src/global/modus-light-theme.scss' statement
+        "src/global/modus-variables.scss", // adds @import 'src/global/modus-variables.scss' statement
+        "src/global/shared-mixins.scss",
+      ],
     }),
   ],
 };
